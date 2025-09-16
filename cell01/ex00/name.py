34d31c65c6cb83d17f3f21e2256ce92e35c4f893
kunneraaaa <65050676@kmitl.ac.th>
2025-09-16 11:07:11 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Pattaraphon"
+last_name = "Thanyanakorn"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Pattaraphon"
+last_name = "Thanyanakorn"
+whole_name = first_name + " " + last_name
+print(whole_name)
